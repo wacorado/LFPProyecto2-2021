0eed54dab22e2d9943949c3b80d939625dc9da1c
wacorado <58500299+wacorado@users.noreply.github.com>
2021-04-29 10:56:24 -0600

    Opcion3
    Ya tengo la info para generar automatas

--- Proyecto3/Proyecto.py
+++ Proyecto3/Proyecto.py
@@ -7,6 +7,9 @@ listaLineasArchivo=[]
 listadeListasGramatica=[]
 listaGramaticasT=[]
 listaGramaticasTc=[]
+listaGramaticasProducciones=[]
+listaInfoGramaticas=[]
+listaGramaticaAutoPila=[]
 def menu():
     #-------------- Funcion que imprime las posibles opciones a acceder y retorna el valor ingresado -----------------------------------
     print("1. Carga de Archivo")
@@ -126,6 +129,112 @@ def produccionesGramaticas():
     for obj in listaGramaticasT:
         print(obj)
         print("\n")
+    #66listaGramaticasProducciones=list(listaGramaticasTc)
+
+def mostrarInfoGramaticas():
+    indexExterno = len(listaGramaticasT)
+    for x in range(indexExterno):
+        indexMedio = len(listaGramaticasT[x])
+        for y in range (indexMedio):
+            if(y==0):
+                CadenaMostrarGram = "Nombre Gramatica: "+str(listaGramaticasT[x][y])+" \n"
+            elif(y==1):
+                indexInterno1=len(listaGramaticasT[x][y])
+                for z in range(indexInterno1):
+                    if(z==0):
+                        CadenaMostrarGram=CadenaMostrarGram+"   No Terminales: {"+str(listaGramaticasT[x][y][z])+"} \n"
+                    elif(z==1):
+                        CadenaMostrarGram=CadenaMostrarGram+"       Terminales: {"+str(listaGramaticasT[x][y][z])+"} \n"
+                    elif(z==2):
+                        CadenaMostrarGram=CadenaMostrarGram+"           No Terminal Inicial: {"+str(listaGramaticasT[x][y][z])+"} \n"
+            elif(y==2):
+                CadenaMostrarGram=CadenaMostrarGram+"                       Producciones:  \n"
+                indexInterno2=len(listaGramaticasT[x][y])
+                for z in range(indexInterno2):
+                    CadenaMostrarGram=CadenaMostrarGram+"                         "+str(listaGramaticasT[x][y][z])+" \n"
+    print(CadenaMostrarGram)
+
+def generarAutomata():
+
+    nombreAutomata="AP_"
+    terminales=""
+    alfabetoPila=""
+    estados="Estados = {i,p,q,f}"
+    estadoInicio="Estado Inicio = { i }"
+    estadoFin="Estado Fin = { f }"
+    listaProducciones=[]
+
+    listaGramaticasTc2= deepcopy(listaGramaticasT)
+    indexPrincipal = len (listaGramaticasTc2)
+    for x in range(indexPrincipal):
+        indexInterno = len(listaGramaticasTc2[x])
+        for y in range(indexInterno):
+            if(y==2):
+                tempProducciones = list(listaGramaticasTc2[x][y])
+                indexProducciones = len(tempProducciones)
+                for z in range(indexProducciones):
+                    cadenaProduccion = str(listaGramaticasTc2[x][y][z]).split("->")
+                    cadenaProduccion2=(str(cadenaProduccion).split(","))
+                    print(str(cadenaProduccion2[1]))
+                    print(str(cadenaProduccion2[0].replace("[\"[\'","")))
+                    indexProdAuto = len(cadenaProduccion2)
+                    for w in range(indexProdAuto):
+                        if(w==0):
+                            #indexProdAuto2=len(cadenaProduccion2[w])
+                            cadenaTemp1=str(cadenaProduccion2[w].replace("[\"[\'",""))
+                            cadenaTemp2=cadenaTemp1.replace("\"","")
+                            cadProdTempAuto="λ,"+cadenaTemp2+";"
+                        elif(w==(indexProdAuto-1)):
+                            cadenaTemp3=str(cadenaProduccion2[w].replace(" \"",""))
+                            cadenaTemp4=cadenaTemp3.replace("\']\"]","")
+                            cadenaTemp7=cadenaTemp4.replace("\']]","")
+                            cadenaTemp9=cadenaTemp7.replace(" \'","")
+                            cadProdTempAuto=cadProdTempAuto+cadenaTemp9
+                        else:
+                            cadenaTemp5=str(cadenaProduccion2[w].replace(" \"",""))
+                            cadenaTemp6=cadenaTemp5.replace(" \'","")
+                            cadenaTemp8=cadenaTemp6.replace("\'","")
+                            cadProdTempAuto=cadProdTempAuto+cadenaTemp8
+                    
+                    #producciones.append(cadenaProduccion2)1
+
+                    listaProducciones.append(cadProdTempAuto)
+                    cadProdTempAuto=""
+  
+    indexExterno = len(listaGramaticasT)
+    for x in range(indexExterno):
+        indexMedio = len(listaGramaticasT[x])
+        for y in range (indexMedio):
+            if(y==0):
+                nombreAutomata=nombreAutomata+str(listaGramaticasT[x][y])
+            elif(y==1):
+                indexInterno1=len(listaGramaticasT[x][y])
+                for z in range(indexInterno1):
+                    if(z==0):
+                        noTerminales=str(listaGramaticasT[x][y][z])
+                    elif(z==1):
+                        terminales=terminales+"Terminales = {"+str(listaGramaticasT[x][y][z])+"} \n"
+                        alfabetoPila=alfabetoPila+"Alfabeto de Pila = {"+str(listaGramaticasT[x][y][z])+","+noTerminales+", #"
+        print(nombreAutomata)
+        print(terminales)
+        print(alfabetoPila)
+        print(estados)
+        print(estadoInicio)
+        print(estadoFin)
+        for obj in listaProducciones:
+            print(obj)
+        cadenaFinalProduccionAutomataPila=nombreAutomata+";"+terminales+";"+alfabetoPila+";"+estados+";"+estadoInicio+";"+estadoFin+";"+str(listaProducciones)
+        listaGramaticaAutoPila.append(cadenaFinalProduccionAutomataPila)
+        listaProducciones.clear()  
+    
+    for obj in listaGramaticaAutoPila:
+        print(obj)
+
+
+def pruebaImprimir():
+    print(int(len(listaGramaticasProducciones)))
+    for obj in listaGramaticasTc:
+        print(obj)
 
 
 def salir():
@@ -148,11 +257,11 @@ while(ciclo):
         
     elif numero == "2":
         print("Opcion2")
-        
+        mostrarInfoGramaticas()
         input("")
     elif numero == "3":
-        print("Opcion3")
-        
+        print("")
+        generarAutomata()
         input("")
     elif numero == "4":
         print("Opcion4")
